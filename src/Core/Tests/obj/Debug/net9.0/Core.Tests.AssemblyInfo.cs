615eeedbda041191a5310192d25b05651e852feb
mustapha-hamze <mustapha.hamze@outlook.com>
2026-08-13 04:21:13 +0330

    general commit aug 13 02

--- src/Core/Tests/obj/Debug/net9.0/Core.Tests.AssemblyInfo.cs
+++ src/Core/Tests/obj/Debug/net9.0/Core.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff30e861edeb4babff2bb00579ed50e5041274f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857bbed942749f3b9ffb24d5140cf860ac14599b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
